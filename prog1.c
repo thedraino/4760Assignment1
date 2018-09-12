70986d898a8f231d94fc74d76ae9f6bfd6176ae8
thedraino <43102350+thedraino@users.noreply.github.com>
2018-09-12 14:12:27 -0500

    Update prog1.c

--- prog1.c
+++ prog1.c
@@ -28,9 +28,11 @@ int main (int argc, char *argv[]) {
           if (childpid = fork())
             break;
         }
-        
-        wait();
-        fprintf (stderr, "i:%d  process ID:%ld  parent ID:%ld  child ID:%ld\n", i, (long)getpid(), (long)getppid(), (long)childpid);
+       
+        fprintf (stderr, "i:%d", i);  
+        fprintf (stderr, "process ID:%ld", (long)getpid());
+        fprintf (stderr, "parent ID:%ld", (long)getppid());  
+        fprintf (stderr, "child ID:%ld\n", (long)childpid);
           
       case 'p' :
         fprintf(stderr, "%s:", argv[0]);
